d8ca46b3b7952315b360032e99393abbaf80ecbd
FransergioDev <fransergio.dev@gmail.com>
2023-08-01 00:26:22 -0300

    Challenge part 6 solution and fixes for part 5

--- app.ts
+++ app.ts
@@ -3,16 +3,28 @@ import { PeopleAccount } from './class/PeopleAccount'
 import { UniversityAccount } from './class/UniversityAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+console.log(peopleAccount)
 peopleAccount.deposit(25)
 peopleAccount.withdraw(5)
-console.log(peopleAccount)
+console.log(peopleAccount.getName())
+peopleAccount.getBalance();
 
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+console.log("\n")
+console.log(companyAccount)
 companyAccount.deposit(27)
 companyAccount.withdraw(15)
+console.log(companyAccount.getName())
 companyAccount.getLoan(10000);
-console.log(companyAccount)
+companyAccount.getBalance();
+
 
 const universityAccount: UniversityAccount = new UniversityAccount('Fransérgio', 30)
-universityAccount.deposit(110);
+console.log("\n")
 console.log(universityAccount)
+universityAccount.depositPremium(100);
+universityAccount.getBalance();
+universityAccount.deposit(100);
+universityAccount.withdraw(25);
+console.log(universityAccount.getName())
+universityAccount.getBalance();
--- class/UniversityAccount.ts
+++ class/UniversityAccount.ts
@@ -1,16 +1,13 @@
 import { DioAccount } from "./DioAccount"
 
 export class UniversityAccount extends DioAccount {
+
     constructor(name: string, accountNumber: number){
         super(name, accountNumber)
     }
 
-    getBalance = (): void => {
-        super.getBalance();
-    }
-
     depositPremium = (valueForDeposit: number): void => {
         const additionalValue = 10;
-        super.deposit(valueForDeposit + additionalValue)
+        this.deposit(valueForDeposit + additionalValue)
     }
 }  
